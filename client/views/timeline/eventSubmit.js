cbaaddffa6301f1459ca60685198b0ddf56f04c3
fadykalo <fadykalo@gmail.com>
2014-05-25 23:56:49 +0100

    Draft of the srver method to submit a new event.

---
+++ client/views/timeline/eventSubmit.js
@@ -0,0 +1,20 @@
+Template.eventSubmit.events({
+  'submit form': function (e) {
+    e.preventDefault();
+    var event = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val(),
+      summary: $(e.target).find('[name=summary]').val(),
+      start: $(e.target).find('[name=start]').val(),
+      end: $(e.target).find('[name=end]').val(),
+      participants: $(e.target).find('[name=participants]').val()
+    }
+
+    Meteor.call('post', post, function (error, id) {
+      if (error) {
+        // display the error to the user
+      } else {
+      }
+    });
+  }
+});
--- collections/events.js
+++ collections/events.js
@@ -1,1 +1,21 @@
+/*globals Meteor*/
 Events = new Meteor.Collection('events');
+
+Meteor.methods({
+  createEvent: function (eventAttributes) {
+
+    var event = _.extend(eventAttributes, {
+      title: eventAttributes.title,
+      userId: user._id,
+      author: user.username,
+      time: new Date().getTime(),
+      summary: eventAttributes.summary,
+      start: eventAttributes.start,
+      end: eventAttributes.end,
+      participants: eventAttributes.participants
+    });
+
+    var eventId = Events.insert(event);
+    return eventId;
+  }
+});
